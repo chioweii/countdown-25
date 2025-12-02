c570179fb38690a0677b51fb5cd8d59978bb2e95
chioweii <rocio442hc@gmail.com>
2025-12-02 08:56:07 +0100

    Update sketch.js

--- sketches/sample-sequence-1/sketch.js
+++ sketches/sample-sequence-1/sketch.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("wall");
 const ctx = canvas.getContext("2d");
+
 const dpr = window.devicePixelRatio || 1;
 canvas.width = window.innerWidth * dpr;
 canvas.height = window.innerHeight * dpr;
